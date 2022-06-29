75feb916f5342fd6ce80832a2b351fa191a2a1bd
RacerShellos107 <108369064+RacerShellos107@users.noreply.github.com>
2022-06-29 10:12:52 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+scene.setBackgroundColor(13)
+let Frog = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Frog.setScale(0.8, ScaleAnchor.Middle)
+animation.runImageAnimation(
+Frog,
+assets.animation`myAnim`,
+100,
+true
+)
